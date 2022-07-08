53f98f82232758fc99e5e8fd3b12f4c1178720f9
sousaGab <gabrielamaralsousa@gmail.com>
2022-07-08 07:34:50 -0300

    token verify url

--- authentication/urls.py
+++ authentication/urls.py
@@ -10,9 +10,9 @@ urlpatterns = [
     #path('token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     #path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
     #allow API users to verify HMAC-signed tokens without having access to your signing key:
-    #path('api/token/verify/', jwt_views.TokenVerifyView.as_view(), name='token_verify'),
     path('', include(router.urls)),
     path('login/', views.LoginAPIView.as_view(), name='login'),
+    path('token/verify/', jwt_views.TokenVerifyView.as_view(), name='token_verify'),
     path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
     path('register/', views.RegisterAPIView.as_view(), name='register')
 ]
