1cb23811afbacefa8a2cc158277398bd6ae4adaa
sousaGab <gabrielamaralsousa@gmail.com>
2023-03-07 19:24:39 -0300

    finish experiment tests

--- experiment/tests/test_views.py
+++ experiment/tests/test_views.py
@@ -4,6 +4,7 @@ from rest_framework.exceptions import ErrorDetail
 from django.urls import reverse
 from experiment.models import Experiment
 from django.contrib.auth.models import User
+from django.forms.models import model_to_dict
 import json
 
 class TestViews(APITestCase):
@@ -94,8 +95,21 @@ class TestViews(APITestCase):
         
         response = self.client.post(self.list_url, self.new_data)
         
+        experiment_created = Experiment.objects.get(id = response.data['id'])
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Experiment.objects.all().count(), self.amount_of_experiments + 1)
+        self.assertEqual(model_to_dict(experiment_created), response.data)
+        self.client.force_authenticate(user=None)
+        
+    def test_experiment_POST_without_data(self):
+        '''
+        This tests whether sending POST request in experiment without data is not allowed
+        '''
+        self.client.force_authenticate(user=self.admin_user)
+        
+        response = self.client.post(self.list_url, data={})
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(Experiment.objects.all().count(), self.amount_of_experiments)
         self.client.force_authenticate(user=None)
     
     #PUT
@@ -126,9 +140,23 @@ class TestViews(APITestCase):
         This tests whether sending PUT request in experiment with permission is successful
         '''
         self.client.force_authenticate(user=self.admin_user)
+        previous_experiment = self.experiment_1
         url = self.list_url + str(self.experiment_1.pk)+ '/'
         response = self.client.put(url, data=self.update_data)
+        updated_experiment = Experiment.objects.get(pk = self.experiment_1.pk)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertNotEqual(model_to_dict(previous_experiment), model_to_dict(updated_experiment))
+        self.assertEqual(response.data, model_to_dict(updated_experiment))
+        self.client.force_authenticate(user=None)
+        
+    def test_experiment_PUT_without_data(self):
+        '''
+        This tests whether sending PUT request in experiment without data is not allowed
+        '''
+        self.client.force_authenticate(user=self.admin_user)
+        url = self.list_url + str(self.experiment_1.pk)+ '/'
+        response = self.client.put(url, data={})
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.client.force_authenticate(user=None)
     
     #PATCH   
@@ -159,11 +187,15 @@ class TestViews(APITestCase):
         This tests whether sending PATCH request in experiment with permission is successful
         '''
         self.client.force_authenticate(user=self.admin_user)
+        previous_experiment = self.experiment_1
         url = self.list_url + str(self.experiment_1.pk)+ '/'
         response = self.client.patch(url, data=self.path_data)
+        updated_experiment = Experiment.objects.get(pk = self.experiment_1.pk)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.client.force_authenticate(user=None)  
-    
+        self.assertNotEqual(model_to_dict(previous_experiment), model_to_dict(updated_experiment))
+        self.assertEqual(response.data, model_to_dict(updated_experiment))
+        self.client.force_authenticate(user=None)
+        
     #DELETE   
     def test_experiment_DELETE_without_authorization(self):
         '''
