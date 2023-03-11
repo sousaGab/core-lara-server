e31a57976206e05d44bb39ca9136b11db4fde8de
sousaGab <gabrielamaralsousa@gmail.com>
2023-03-11 03:08:32 -0300

    update user url

--- user/urls.py
+++ user/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework import routers
-from . import views
+from user import views
 
 router = routers.DefaultRouter()
 router.register(r'', views.UserViewSet, basename='Users')
